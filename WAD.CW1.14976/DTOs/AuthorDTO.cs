9cc9d1bd0e29e01ee513dd6a16131aa76211c179
14976wiut <wiut14976@gmail.com>
2024-06-02 19:31:38 +0500

    Data trasfer models

---
+++ WAD.CW1.14976/DTOs/AuthorDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WAD.CW1._14976.DTOs
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Nationality { get; set; }
+        public ICollection<BookDTO> Books { get; set; }
+    }
+}
---
+++ WAD.CW1.14976/DTOs/BookDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace WAD.CW1._14976.DTOs
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public string Genre { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
