3d03dc03c0b1e463ba67558686bd84e5f3f7e40c
14976wiut <wiut14976@gmail.com>
2024-06-02 19:31:55 +0500

    Mapping

---
+++ WAD.CW1.14976/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using WAD.CW1._14976.DTOs;
+using WAD.CW1._14976.Models;
+
+namespace WAD.CW1._14976.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Author, AuthorDTO>()
+                .ForMember(dest => dest.Books, opt => opt.MapFrom(src => src.Books));
+
+            CreateMap<Book, BookDTO>()
+                .ForMember(dest => dest.AuthorName, opt => opt.MapFrom(src => src.Author.Name));
+
+            CreateMap<AuthorDTO, Author>();
+            CreateMap<BookDTO, Book>();
+        }
+    }
+}
